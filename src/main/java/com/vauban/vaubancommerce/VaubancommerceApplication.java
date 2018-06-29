126b4e2682645d136989bdec992099fad8dd0021
Stênio Galvão <steniogalvao@gmail.com>
2018-06-29 19:55:24 -0300

    First commit

---
+++ src/main/java/com/vauban/vaubancommerce/VaubancommerceApplication.java
@@ -0,0 +1,12 @@
+package com.vauban.vaubancommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaubancommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaubancommerceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vauban/vaubancommerce/VaubancommerceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vauban.vaubancommerce;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VaubancommerceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
